ce497bfb38f499fd45ae16428fee6dfecc89d3df
shahisrail <shahisrail134@gmail.com>
2025-01-28 01:25:42 +0600

    some updated

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -70,11 +70,11 @@ const OrganizationStats = () => {
   ];
 
   return (
-    <div className="grid grid-cols-1 items-center lg:grid-cols-2 gap-8 font-bold  justify-around font-sans p-6">
+    <div className="grid grid-cols-1 items-center lg:grid-cols-2 gap-8 font-bold justify-around font-sans p-6">
       {/* Organization Table */}
-      <div className=" border-gray-300 rounded-lg overflow-hidden pt-[44px]">
-        <table className="table-auto w-full text-left ">
-          <thead className=" text-gray-700">
+      <div className=" rounded-lg overflow-hidden pt-[41px]">
+        <table className="table-auto w-full text-left">
+          <thead className="text-gray-700">
             <tr>
               <th className="px-4 text-[20px] py-2">Organization</th>
               <th className="border-l-2 border-black px-4 py-2"></th>
@@ -98,16 +98,16 @@ const OrganizationStats = () => {
       </div>
 
       {/* Percentile Charts */}
-      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {chartData.map((chart, index) => (
           <div key={index} className="text-center p-4">
-            <h4 className="text-xl  mb-4 font-bold" >{chart.title}</h4>
-            <h4 className="text-sm  mb-4 font-bold" >50Th</h4>
+            <h4 className="text-xl mb-4 font-bold">{chart.title}</h4>
+            <h4 className="text-sm mb-4 font-bold">50Th</h4>
 
             <div className="overflow-x-auto">
               <div className="border-t-2 border-black overflow-hidden">
-                <table className="table-auto w-full border border-gray-300  text-center">
-                  <tbody>
+                <table className="table-auto w-full border-x-2 border-gray-300 text-center">
+                  <tbody className="">
                     {/* Manually creating 3 rows */}
                     <tr className="border-b border-gray-300">
                       <td className="px-4 py-4 border-r border-gray-300">
@@ -139,7 +139,7 @@ const OrganizationStats = () => {
                       </td>
                       <td className="px-4 py-4">
                         <div
-                          className={`w-4 h-4 rounded-full font-bold mx-auto border-2  ${
+                          className={`w-4 h-4 rounded-full font-bold mx-auto border-2 ${
                             chart.values[3]
                               ? "border-[#7ABFF9] bg-[#7ABFF9]"
                               : "border-gray-300"
@@ -178,9 +178,8 @@ const OrganizationStats = () => {
                         ></div>
                       </td>
                       <td className="px-4 py-6 relative">
-                        {" "}
                         <div
-                          className={`w-4 h-4  absolute top-[20px] left-0 ml-[-9px] rounded-full font-bold border-2  ${
+                          className={`w-4 h-4 absolute top-[20px] left-0 ml-[-9px] rounded-full font-bold border-2 ${
                             chart.values[7]
                               ? "border-[#7ABFF9] bg-[#7ABFF9]"
                               : "border-gray-300 bg-gray-300"
@@ -220,7 +219,7 @@ const OrganizationStats = () => {
                       </td>
                       <td className="px-4 py-4">
                         <div
-                          className={`w-4 h-4 rounded-full mx-auto border-2  ${
+                          className={`w-4 h-4 rounded-full mx-auto border-2 ${
                             chart.values[11]
                               ? "border-[#7ABFF9] bg-[#7ABFF9]"
                               : "border-gray-300 bg-gray-300"
